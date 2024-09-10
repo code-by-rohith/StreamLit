9b365606eacc45d69efab1067b107304952ad4c8
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-09-10 18:16:39 +0530

    Add

--- DataFrame.py
+++ DataFrame.py
@@ -1,3 +1,5 @@
-import streamlit as st
-
-st.markdown("[Go to Google](https://www.google.com)")
+import pandas as pd
+import  streamlit as st
+st.title("Diabetic Data Base")
+s=pd.read_csv("diabetics.csv")
+st.write(s)
