cef8cb320c033b8690691eba2512eb27f4617c1d
R.O.H.I.T.H <168300495+code-by-rohith@users.noreply.github.com>
2024-10-18 12:47:09 +0530

    adfd

--- DataFrame.py
+++ DataFrame.py
@@ -3,7 +3,7 @@ import streamlit as st
 import time
 import plotly.express as px
 
-st.title("Diabetes Data Dashboard")
+st.title("Diabetes Data Dashboard PAAS")
 s = pd.read_csv("diabetics.csv")
 with st.spinner("Loading !"):
     time.sleep(6)
